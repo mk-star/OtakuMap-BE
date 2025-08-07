29b9417e6853c1f384baa7b0b2bd33980b129412
mk-star <ssunn0812@gmail.com>
2025-08-07 23:19:05 +0900

    Feat: Redis 캐싱 적용

--- src/main/java/com/otakumap/domain/event/service/EventQueryServiceImpl.java
+++ src/main/java/com/otakumap/domain/event/service/EventQueryServiceImpl.java
@@ -8,6 +8,7 @@ import com.otakumap.domain.search.repository.SearchRepositoryCustom;
 import com.otakumap.global.apiPayload.code.status.ErrorStatus;
 import com.otakumap.global.apiPayload.exception.handler.EventHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,10 @@ public class EventQueryServiceImpl implements EventQueryService{
     private final EventRepository eventRepository;
     private final SearchRepositoryCustom searchRepository;
 
+    @Cacheable(
+            value = "events",
+            key = "#eventId"
+    )
     @Override
     public EventResponseDTO.EventDetailDTO getEventDetail(Long eventId) {
         Event event = eventRepository.findById(eventId).orElseThrow(() -> new EventHandler(ErrorStatus.EVENT_NOT_FOUND));
